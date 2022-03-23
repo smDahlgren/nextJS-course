6fd94eb825718ec5d0430833e1a8a30f973e4aff
Choadis <dahlgrensm@gmail.com>
2022-03-23 17:12:46 -0400

    Limiting component to one page

---
+++ dj-events-frontend/components/Showcase.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from '@/styles/Showcase.module.css'
+
+const Showcase = () => {
+  return (
+    <div className={styles.showcase}>
+      <h1>Enjoy the music</h1>
+      <h2>Find music events</h2>
+    </div>
+  )
+}
+
+export default Showcase
