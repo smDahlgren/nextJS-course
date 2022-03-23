ec70b2197a7addef24b15188ec6d26999caa57f0
Choadis <dahlgrensm@gmail.com>
2022-03-23 16:59:39 -0400

    Added 404 page

---
+++ dj-events-frontend/pages/404.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import styles from '../styles/404.module.css'
+import Link from 'next/link'
+import {FaExclamationTriangle} from 'react-icons/fa'
+
+const NotFoundPage = () => {
+  return (
+    <Layout title='Page not found'>
+      <div className={styles.error}>
+        <h1>
+          <FaExclamationTriangle />404
+        </h1>
+        <p>Nothing is here</p>
+        <Link href='/'>Home</Link>
+      </div>
+    </Layout>
+  )
+}
+
+export default NotFoundPage
